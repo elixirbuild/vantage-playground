75db4b1b452b22bbb1da11500aaa8de55e9162cf
Vantage <63171099+elixirbuild@users.noreply.github.com>
2021-06-05 12:55:21 -0500

    Update fps.js

--- editor/components/fps.js
+++ editor/components/fps.js
@@ -1,1 +1,18 @@
+const times = [];
+let fps;
 
+function refreshLoop() {
+  window.requestAnimationFrame(() => {
+    const now = performance.now();
+    while (times.length > 0 && times[0] <= now - 1000) {
+      times.shift();
+    }
+    times.push(now);
+    fps = times.length;
+
+    document.getElementById("fps").innerHTML = fps;
+    refreshLoop();
+  });
+}
+
+refreshLoop();
